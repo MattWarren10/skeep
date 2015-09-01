4da5b464588e3ca54371c692aabad1c7d5a1e80c
MattWarren10 <mwarren@harding.edu>
2015-09-01 07:20:23 -0400

    add prompt which asks user maximum possible score.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
-
+//asks user winning score
+var inputMaxScore = prompt("What is the winning score?");
+var maxScore = parseInt(inputMaxScore);
 
 //beginning score
 var score = 0;
-while (score < 10) {
+while (score < maxScore) {
   var input = prompt("How many points did you score?");
   var points = parseInt(input);
   var totalScore = score += points;
